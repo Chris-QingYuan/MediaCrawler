45d68276a6970491a8d3d5fe87d2194c3ad9d08b
Qing Yuan <1515y.chris@gmail.com>
2023-11-28 22:16:09 -0500

    U: xhs.client - loosen error handling, code formatting

--- media_platform/xhs/client.py
+++ media_platform/xhs/client.py
@@ -14,13 +14,13 @@ from .help import get_search_id, sign
 
 class XHSClient:
     def __init__(
-            self,
-            timeout=10,
-            proxies=None,
-            *,
-            headers: Dict[str, str],
-            playwright_page: Page,
-            cookie_dict: Dict[str, str],
+        self,
+        timeout=10,
+        proxies=None,
+        *,
+        headers: Dict[str, str],
+        playwright_page: Page,
+        cookie_dict: Dict[str, str],
     ):
         self.proxies = proxies
         self.timeout = timeout
@@ -34,51 +34,58 @@ class XHSClient:
         self.cookie_dict = cookie_dict
 
     async def _pre_headers(self, url: str, data=None):
-        encrypt_params = await self.playwright_page.evaluate("([url, data]) => window._webmsxyw(url,data)", [url, data])
+        encrypt_params = await self.playwright_page.evaluate(
+            "([url, data]) => window._webmsxyw(url,data)", [url, data]
+        )
         local_storage = await self.playwright_page.evaluate("() => window.localStorage")
         signs = sign(
             a1=self.cookie_dict.get("a1", ""),
             b1=local_storage.get("b1", ""),
             x_s=encrypt_params.get("X-s", ""),
-            x_t=str(encrypt_params.get("X-t", ""))
+            x_t=str(encrypt_params.get("X-t", "")),
         )
 
         headers = {
             "X-S": signs["x-s"],
             "X-T": signs["x-t"],
             "x-S-Common": signs["x-s-common"],
-            "X-B3-Traceid": signs["x-b3-traceid"]
+            "X-B3-Traceid": signs["x-b3-traceid"],
         }
         self.headers.update(headers)
         return self.headers
 
     async def request(self, method, url, **kwargs) -> Dict:
         async with httpx.AsyncClient(proxies=self.proxies) as client:
-            response = await client.request(
-                method, url, timeout=self.timeout,
-                **kwargs
-            )
+            response = await client.request(method, url, timeout=self.timeout, **kwargs)
         data: Dict = response.json()
         if data["success"]:
             return data.get("data", data.get("success", {}))
         elif data["code"] == self.IP_ERROR_CODE:
             raise IPBlockError(self.IP_ERROR_STR)
         else:
+            utils.logger.error(
+                "Fetch data failed for url %s, with payload %s, got data: %s",
+                url,
+                kwargs.get("data", None),
+                data,
+            )
             raise DataFetchError(data.get("msg", None))
 
     async def get(self, uri: str, params=None) -> Dict:
         final_uri = uri
         if isinstance(params, dict):
-            final_uri = (f"{uri}?"
-                         f"{'&'.join([f'{k}={v}' for k, v in params.items()])}")
+            final_uri = f"{uri}?" f"{'&'.join([f'{k}={v}' for k, v in params.items()])}"
         headers = await self._pre_headers(final_uri)
-        return await self.request(method="GET", url=f"{self._host}{final_uri}", headers=headers)
+        return await self.request(
+            method="GET", url=f"{self._host}{final_uri}", headers=headers
+        )
 
     async def post(self, uri: str, data: dict) -> Dict:
         headers = await self._pre_headers(uri, data)
-        json_str = json.dumps(data, separators=(',', ':'), ensure_ascii=False)
-        return await self.request(method="POST", url=f"{self._host}{uri}",
-                                  data=json_str, headers=headers)
+        json_str = json.dumps(data, separators=(",", ":"), ensure_ascii=False)
+        return await self.request(
+            method="POST", url=f"{self._host}{uri}", data=json_str, headers=headers
+        )
 
     async def ping(self) -> bool:
         """get a note to check if login state is ok"""
@@ -99,10 +106,12 @@ class XHSClient:
         self.cookie_dict = cookie_dict
 
     async def get_note_by_keyword(
-            self, keyword: str,
-            page: int = 1, page_size: int = 20,
-            sort: SearchSortType = SearchSortType.GENERAL,
-            note_type: SearchNoteType = SearchNoteType.ALL
+        self,
+        keyword: str,
+        page: int = 1,
+        page_size: int = 20,
+        sort: SearchSortType = SearchSortType.GENERAL,
+        note_type: SearchNoteType = SearchNoteType.ALL,
     ) -> Dict:
         """search note by keyword
 
@@ -120,7 +129,7 @@ class XHSClient:
             "page_size": page_size,
             "search_id": get_search_id(),
             "sort": sort.value,
-            "note_type": note_type.value
+            "note_type": note_type.value,
         }
         return await self.post(uri, data)
 
@@ -142,16 +151,11 @@ class XHSClient:
         :return: {"has_more": true,"cursor": "6422442d000000000700dcdb",comments: [],"user_id": "63273a77000000002303cc9b","time": 1681566542930}
         """
         uri = "/api/sns/web/v2/comment/page"
-        params = {
-            "note_id": note_id,
-            "cursor": cursor
-        }
+        params = {"note_id": note_id, "cursor": cursor}
         return await self.get(uri, params)
 
     async def get_note_sub_comments(
-            self, note_id: str,
-            root_comment_id: str,
-            num: int = 30, cursor: str = ""
+        self, note_id: str, root_comment_id: str, num: int = 30, cursor: str = ""
     ):
         """
         get note sub comments
@@ -170,7 +174,9 @@ class XHSClient:
         }
         return await self.get(uri, params)
 
-    async def get_note_all_comments(self, note_id: str, crawl_interval: float = 1.0, is_fetch_sub_comments=False):
+    async def get_note_all_comments(
+        self, note_id: str, crawl_interval: float = 1.0, is_fetch_sub_comments=False
+    ):
         """
         get note all comments include sub comments
         :param note_id:
@@ -183,7 +189,10 @@ class XHSClient:
         comments_has_more = True
         comments_cursor = ""
         while comments_has_more:
-            comments_res = await self.get_note_comments(note_id, comments_cursor)
+            try:
+                comments_res = await self.get_note_comments(note_id, comments_cursor)
+            except DataFetchError:  # 评论接口返回异常，直接返回空列表
+                return result
             comments_has_more = comments_res.get("has_more", False)
             comments_cursor = comments_res.get("cursor", "")
             # Check if 'comments' key exists in the response
@@ -203,15 +212,20 @@ class XHSClient:
                 cur_sub_comment_count = int(comment["sub_comment_count"])
                 cur_sub_comments = comment["sub_comments"]
                 result.extend(cur_sub_comments)
-                sub_comments_has_more = comment["sub_comment_has_more"] and len(
-                    cur_sub_comments) < cur_sub_comment_count
+                sub_comments_has_more = (
+                    comment["sub_comment_has_more"]
+                    and len(cur_sub_comments) < cur_sub_comment_count
+                )
                 sub_comment_cursor = comment["sub_comment_cursor"]
                 while sub_comments_has_more:
                     page_num = 30
-                    sub_comments_res = await self.get_note_sub_comments(note_id, comment["id"], num=page_num,
-                                                                        cursor=sub_comment_cursor)
+                    sub_comments_res = await self.get_note_sub_comments(
+                        note_id, comment["id"], num=page_num, cursor=sub_comment_cursor
+                    )
                     sub_comments = sub_comments_res["comments"]
-                    sub_comments_has_more = sub_comments_res["has_more"] and len(sub_comments) == page_num
+                    sub_comments_has_more = (
+                        sub_comments_res["has_more"] and len(sub_comments) == page_num
+                    )
                     sub_comment_cursor = sub_comments_res["cursor"]
                     result.extend(sub_comments)
                     await asyncio.sleep(crawl_interval)
